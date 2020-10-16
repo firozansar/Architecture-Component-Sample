2db9b7d59b7b1655fcb4c645eab64c956dbb0055
Firoz Ansari <firozansari@stagecoach-technology.com>
2020-10-16 04:28:06 +0100

    Recycler item view logic implemented

--- app/src/main/java/info/firozansari/architecture_component/ui/adapter/ArticleViewHolder.kt
+++ app/src/main/java/info/firozansari/architecture_component/ui/adapter/ArticleViewHolder.kt
@@ -7,6 +7,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.bumptech.glide.request.target.Target
+import info.firozansari.architecture_component.Extension.gone
 import info.firozansari.architecture_component.models.Article
 import kotlinx.android.synthetic.main.item_article.view.*
 
@@ -29,13 +30,34 @@ class ArticleViewHolder(parent: View) : RecyclerView.ViewHolder(parent) {
     fun setupViews(it: Article, itemView: View) {
         itemView.blog_user_name_tv.text = it.user[0].name
         itemView.blog_user_des_tv.text = it.user[0].designation
+        itemView.blog_like_count_tv.text = """${getFormattedNumber(it.likes)} Likes"""
+        itemView.blog_comment_count_tv.text = """${getFormattedNumber(it.comments)} Comments"""
+        itemView.blog_content.text = it.content
+
         it.user[0].avatar?.let { it1 ->
             Glide.with(itemView.context).load(it1)
                 .into(itemView.blog_user_image)
-
         }
-////        itemView.recipe_title.text = it.title
-//        itemView.recipe_ingredients.text = it.ingredients
+        if(it.media.isNotEmpty()){
+            Glide.with(itemView.context).load(it.media[0].image)
+                .into(itemView.blog_image)
+        } else {
+            itemView.blog_image.gone()
+        }
+
+    }
+
+    private fun getFormattedNumber(number: Int): String {
+        var numberString = ""
+        numberString = when {
+            Math.abs(number / 1000) > 1 -> {
+                (number / 1000).toString() + "k";
+            }
+            else -> {
+                number.toString();
+            }
+        }
+        return numberString
     }
 
     private fun setListeners(
