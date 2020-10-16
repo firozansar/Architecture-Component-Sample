4dde7ca5de44405ef96bb444a73da3667d92035c
Firoz Ansari <firozansari@stagecoach-technology.com>
2020-10-16 03:21:58 +0100

    Recycler view logic implemented

--- app/src/main/java/info/firozansari/architecture_component/ui/ArticlesFragment.kt
+++ app/src/main/java/info/firozansari/architecture_component/ui/ArticlesFragment.kt
@@ -2,15 +2,24 @@ package info.firozansari.architecture_component.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import info.firozansari.architecture_component.Extension.gone
+import info.firozansari.architecture_component.Extension.visible
 import info.firozansari.architecture_component.R
+import info.firozansari.architecture_component.api.NetworkState
+import info.firozansari.architecture_component.ui.adapter.ArticlesAdapter
+import info.firozansari.architecture_component.viewmodels.ArticlesViewModel
+import kotlinx.android.synthetic.main.fragment_articles.*
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 
-class ArticlesFragment : Fragment() {
+class ArticlesFragment : Fragment(), ArticlesAdapter.OnClickListener {
+
+    private val recyclerViewAdapter = ArticlesAdapter(this)
+    private val model by viewModel<ArticlesViewModel>()
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -22,9 +31,64 @@ class ArticlesFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        setupListeners()
+        observeViewModelData()
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+    }
+
+    private fun setupListeners() {
+        fragment_button_load_data.setOnClickListener { model.refreshAllList() }
+    }
+
+    private fun observeViewModelData() {
+        model.networkState?.observe(viewLifecycleOwner, Observer { recyclerViewAdapter.updateNetworkState(it) })
+        model.recipes.observe(viewLifecycleOwner, Observer { recyclerViewAdapter.submitList(it) })
+    }
+
+    private fun setupRecyclerView() {
+        fragment_recipes_recycler_view.adapter = recyclerViewAdapter
+    }
+
+    //Override onRetryClick from ArticlesAdapter.OnClickListener
+    override fun onRetryClick() {
+        model.refreshFailedRequest()
+    }
+
+    //Override whenListIsUpdated from ArticlesAdapter.OnClickListener
+    override fun whenListIsUpdated(size: Int, networkState: NetworkState?) {
+        setInitialStates()
+        if (size == 0) {
+            setSizeZeroInitialState()
+            when (networkState) {
+                NetworkState.SUCCESS -> {
+                    fragment_text_network.text = getString(R.string.article_empty)
+                    fragment_text_network.visible()
+                    fragment_button_load_data.gone()
+                }
+                NetworkState.FAILED -> {
+                    fragment_text_network.text = getString(R.string.error_msg)
+                    fragment_image_warning.visible()
+                    fragment_text_network.visible()
+                    fragment_button_load_data.visible()
+                }
+                NetworkState.RUNNING -> {
+                    fragment_progress_bar.visible()
+                }
+            }
         }
     }
+
+    private fun setSizeZeroInitialState() {
+        fragment_text_network.text = getString(R.string.article_empty)
+        fragment_text_network.visible()
+    }
+
+    private fun setInitialStates() {
+        fragment_button_load_data.gone()
+        fragment_image_warning.gone()
+        fragment_text_network.gone()
+        fragment_progress_bar.gone()
+    }
+
 }
--- app/src/main/java/info/firozansari/architecture_component/ui/adapter/ArticleViewHolder.kt
+++ app/src/main/java/info/firozansari/architecture_component/ui/adapter/ArticleViewHolder.kt
@@ -1,13 +1,20 @@
 package info.firozansari.architecture_component.ui.adapter
 
+import android.R
+import android.graphics.drawable.Drawable
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import info.firozansari.architecture_component.models.Article
+import kotlinx.android.synthetic.main.item_article.view.*
+
 
 /**
  * Created by Firoz Ansari on 15/10/2020.
  */
-class ArticleViewHolder (parent: View) : RecyclerView.ViewHolder(parent) {
+class ArticleViewHolder(parent: View) : RecyclerView.ViewHolder(parent) {
 
     fun bind(
         article: Article?,
@@ -20,7 +27,15 @@ class ArticleViewHolder (parent: View) : RecyclerView.ViewHolder(parent) {
     }
 
     fun setupViews(it: Article, itemView: View) {
+        itemView.blog_user_name_tv.text = it.user[0].name
+        itemView.blog_user_des_tv.text = it.user[0].designation
+        it.user[0].avatar?.let { it1 ->
+            Glide.with(itemView.context).load(it1)
+                .into(itemView.blog_user_image)
 
+        }
+////        itemView.recipe_title.text = it.title
+//        itemView.recipe_ingredients.text = it.ingredients
     }
 
     private fun setListeners(
